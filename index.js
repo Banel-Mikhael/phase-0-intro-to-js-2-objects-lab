912a64c54c906beec09365ce118e8b2ebaf0cee6
banelmikhael <banel.mikhael.bm@gmail.com>
2022-10-24 19:59:44 +0300

    assigment completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+const employee={
+    name: `banel`,
+    streetAddress: `masiki`,
+};
+
+function  updateEmployeeWithKeyAndValue(employee, key, value){
+
+    const newEmpolyee= {...employee}
+    newEmpolyee.name="Sam";
+    newEmpolyee.streetAddress="11 Broadway";
+    return newEmpolyee
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+
+    employee.streetAddress="12 Broadway";
+
+    return employee;
+}
+
+function  deleteFromEmployeeByKey(employee, key){
+
+    const newEmployee ={...employee}
+    delete newEmployee.name
+    delete newEmployee.streetAddress
+    return newEmployee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  
+    delete employee.name
+    employee.streetAddress='masiki'
+    return employee
+}
